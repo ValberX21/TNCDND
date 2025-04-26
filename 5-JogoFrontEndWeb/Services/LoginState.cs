2e7cd6f2192100850f12d5bd31e60e58eff0e12a
Valber Martins <martins_shinigami@hotmail.com>
2025-04-26 15:23:33 -0300

    Added item 5, started development in from web

---
+++ 5-JogoFrontEndWeb/Services/LoginState.cs
@@ -0,0 +1,17 @@
+﻿namespace _5_JogoFrontEndWeb.Services
+{
+    public class LoginState
+    {
+        public bool IsLoggedIn { get; private set; } = false;
+
+        public event Action? OnChange;
+
+        public void Login()
+        {
+            IsLoggedIn = true;
+            NotifyStateChanged();
+        }
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}

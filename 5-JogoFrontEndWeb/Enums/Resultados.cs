841beca717650275a6d0b3c52aab88c828832912
Valber Martins <martins_shinigami@hotmail.com>
2025-04-28 17:34:38 -0300

    Added enums

---
+++ 5-JogoFrontEndWeb/Enums/Resultados.cs
@@ -0,0 +1,14 @@
+﻿namespace _5_JogoFrontEndWeb.Enums
+{
+    public enum TentativaResultado
+    {
+        SUCCESS,
+        WRONG
+    }
+
+    public enum JogoResultado
+    {
+        VITORIA,
+        DERROTA
+    }
+}
